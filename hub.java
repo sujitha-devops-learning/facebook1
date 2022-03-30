815781101c615f257cd8bf045edbe89675f8b3f8
sujitha <sujitha.svuce@gmail.com>
2022-03-30 12:58:13 +0530

    git commands

---
+++ hub.java
@@ -0,0 +1,14 @@
+this is about git commands 
+the commands are git init empty repo
+git add .
+git commit -m "message"
+git remote add aliasname
+git remote -v
+git log 
+git remote remove aliasname
+git status
+git diff
+git show --pretty= "" --name -only commit code
+git reset filename
+git clean -n
+git clean -f
