ac97fb7064bb114181e542ba953f9a8a0975d883
easyinfoall <sujitha.svuce@gmail.com>
2022-04-23 17:04:18 +0530

    deploy file updation

--- deploy.sh
+++ deploy.sh
@@ -3,4 +3,4 @@ this line again updated in master branch
 this file is updated in dev branch
 this file contains deploy logic
 some lines of program is added to deploy.sh
-this file updated to rivision
+this file updated to rivision.......
